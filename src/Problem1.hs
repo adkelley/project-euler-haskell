11b17b8584f2b1664ccc535dc436eb7715e0d66f
Alex Kelley <adkelley@users.noreply.github.com>
2017-01-31 11:32:31 -0800

    problem 1 naming

--- src/Problem1.hs
+++ src/Problem1.hs
@@ -2,13 +2,13 @@ module Problem1
     ( problem1
     ) where
 
-multiples_of_3n5 :: Int -> Bool
-multiples_of_3n5 x = rem x 3 == 0 || rem x 5 == 0
+multiplesOf3or5 :: Int -> Bool
+multiplesOf3or5 x = rem x 3 == 0 || rem x 5 == 0
 
-answer :: [Int]
-answer = filter multiples_of_3n5 [1..10]
+answer ::  Int -> [Int]
+answer maxNum = filter multiplesOf3or5 [1..(maxNum-1)]
 
 problem1 :: IO ()
 -- problem1 = putStrLn "Now I'm watching the build and saying something"
 problem1 =
-  print answer
+  print $ answer 1000
